641e9ba13d8803ac89e05c9489eb0798435ca47b
Artur Baruchi <artur.baruchi@gmail.com>
2016-06-29 17:11:53 -0300

    Configuration in run.py to load and setup the application to use token.

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from api import users
 from api import napps
 from api import comments
 from api import common
+from datetime import timedelta
 
 # Expose user endpoints
 common.app.register_blueprint(users.api)
@@ -18,3 +19,8 @@ common.app.register_blueprint(comments.api)
 
 if __name__ == '__main__':
     common.app.run(debug=True)
+    common.app.config["REMEMBER_COOKIE_DURATION"] = timedelta(days=14)
+
+    common.login_manager.login_view = "/login/"
+    common.login_manager.setup_app(common.app)
+    common.app.run()
