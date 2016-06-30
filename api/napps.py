d91ec11ed7ca04fafa2075415ffa1a16ffb3d744
Artur Baruchi <artur.baruchi@gmail.com>
2016-06-30 13:07:46 -0300

    Added a login.html to evaluate the login process with tokens - Issue #3

--- api/napps.py
+++ api/napps.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 from flask import request
 from flask import redirect
 from flask import jsonify
+from flask import render_template
 from flask.ext.login import login_required
 from flask_login import login_user
 
@@ -38,13 +39,14 @@ def login_page():
     Endpoint to perform the authentication
     :return:
     """
-    #print(request.method)
     if request.method == "POST":
         user = common.User.get(request.form['username'])
 
         if user and common.hash_pass(request.form['password']) == user.password:
             login_user(user, remember=True)
             return redirect(request.args.get("next") or "/napps/")
+
+    return render_template("login.html")
 
 
 @api.route('/napps/', methods=['GET'])
