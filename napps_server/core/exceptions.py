47369a2211090712253824cd0af82a960a27211c
Macartur Sousa <macartur.sc@gmail.com>
2016-12-20 10:35:11 -0200

    Fix lint from napps_server/core/exceptions.py

--- napps_server/core/exceptions.py
+++ napps_server/core/exceptions.py
@@ -1,18 +1,31 @@
+"""Module with main Exceptions used by napps-server."""
+
+
 class NappsDuplicateEntry(Exception):
+    """Exception thrown when found duplicated napps."""
+
     pass
 
 
 class NappsEntryDoesNotExists(Exception):
+    """Exception thrown when not found a entry to napp."""
+
     pass
 
 
 class InvalidNappMetaData(Exception):
+    """Exception thrown when napp have a invalid metadata."""
+
     pass
 
 
 class InvalidAuthor(Exception):
+    """Exception thrown when a invalid author is found."""
+
     pass
 
 
 class RepositoryNotReachable(Exception):
+    """Exception thrown when repository can be found."""
+
     pass
