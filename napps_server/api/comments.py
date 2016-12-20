599d460cce653cba8fc48b6e79925ea6a509f100
Macartur Sousa <macartur.sc@gmail.com>
2016-12-20 12:20:46 -0200

    Refactored napps_server/api/comments.py
    - Fixed lint warnings
    - Refactored endpoints to reduce code.

--- napps_server/api/comments.py
+++ napps_server/api/comments.py
@@ -1,3 +1,4 @@
+"""Module used to handle comments from napps."""
 # System imports
 import time
 
@@ -14,24 +15,42 @@ api = Blueprint('comments_api', __name__)
 
 
 def get_redis_list(name, key):
-     return list(con.smembers(con.hget(name,key)))
+    """Method used to get all members of a given name and key.
+
+    This method get a name and a key, builds a hash and get all values stored
+    by redis using that hash.
+
+    Parameters:
+        name (string): name to be searchable by redis.
+        key (string): key value to be searchable by redis
+
+    Returns:
+        redis_list (list): list with all members of a given name and key.
+    """
+    return list(con.smembers(con.hget(name, key)))
 
 
 def get_comment(comment_key):
-    """
-    This routine receives a key for a given comment and returns the JSON with all comment data.
-    :param comment_key: The key for the comment
-    :return: JSON with structured data of the comment
+    """Get a json with all comment data using a key from a given comment.
+
+    Parameters:
+        comment_key (string): The key from a comment.
+    Returns:
+        json (string): JSON with structured data of the comment.
     """
     return con.hgetall(comment_key)
 
+
 def get_author(author_key):
+    """Get a python dictionary with some basic informations about the author.
+
+    Parameters:
+        author_key (string): The key from a author.
+    Returns:
+        author (dict): python dictonary with author informations.
     """
-    This routine returns a dict with some basic information about the author
-    :param author_key:
-    :return:
-    """
-    exclude = ['apps', 'city', 'comments', 'country', 'pass', 'phone', 'role', 'state', 'status', 'timezone', 'tokens']
+    exclude = ['apps', 'city', 'comments', 'country', 'pass', 'phone', 'role',
+               'state', 'status', 'timezone', 'tokens']
     author_dict = con.hgetall(author_key)
 
     for item in exclude:
@@ -42,44 +61,70 @@ def get_author(author_key):
 
 @api.route('/authors/<name>/comments', methods=['GET'])
 def get_author_comments(name):
+    """Method used to get all comments of a given author.
+
+    This method creates a '/authors/<name>/comments' endpoint to return a JSON
+    with all comments of a given author.
+
+    Parameters:
+        name (string): Name of a author.
+    Returns:
+        json (string): Structured JSON with a list of comments.
     """
-    This endpoints returns an JSON with all comments of a given application
-    """
-    user_comments_dict = {}
-    comment_number = 0
-    user_key = "author:"+name
+    user_key = "author:" + name
     if con.sismember("authors", user_key):
-        user_comment_key = con.hget(user_key, "comments")
-        for comment in list(con.smembers(user_comment_key)):
-            user_comments_dict[comment_number] = get_comment(comment)
-            author_data = get_author(user_comments_dict[comment_number]["author"])
-            user_comments_dict[comment_number]["author"] = author_data
-            comment_date = time.strptime(user_comments_dict[comment_number]["datetime"],'%Y%d%m%H%M')
-            user_comments_dict[comment_number]["datetime"] = time.strftime("%a, %d %b %Y %H:%M", comment_date)
-            comment_number = comment_number + 1
-        return jsonify({'comments': user_comments_dict})
-    else:
-        return None
+        return get_all_comments(user_key)
 
 
 @api.route('/napps/<name>/comments', methods=['GET'])
 def get_napps_comments(name):
+    """Method used to get all comments of a given Network Application.
+
+    This method creates a '/napps/<name>/comments' endpoint to return a JSON
+    with all comments of a given Network Application.
+
+    Parameters:
+        name (string): Name of Network Application.
+    Returns:
+        json (string): Structured JSON with all comments.
     """
-    This endpoints returns a JSON with all comments of a given author.
-    """
-    app_comments_dict = {}
-    comment_number = 0
-    app_key = "app:"+name
-    print(app_key)
+    app_key = "app:" + name
     if con.sismember("apps", app_key):
-        app_comment_key = con.hget(app_key, "comments")
-        for comment in list(con.smembers(app_comment_key)):
-            app_comments_dict[comment_number] = get_comment(comment)
-            author_data = get_author(app_comments_dict[comment_number]["author"])
-            app_comments_dict[comment_number]["author"] = author_data
-            comment_date = time.strptime(app_comments_dict[comment_number]["datetime"],'%Y%d%m%H%M')
-            app_comments_dict[comment_number]["datetime"] = time.strftime("%a, %d %b %Y %H:%M", comment_date)
-            comment_number = comment_number + 1
-        return jsonify({'comments': app_comments_dict})
-    else:
-        return None
+        return get_all_comments(app_key)
+
+
+def get_all_comments(key):
+    """Method used to get all comments of a given key.
+
+    Parameters:
+        key (string): String key stored by redis.
+    Returns:
+        json (string): JSON Structured with all comments of a given key.
+    """
+    comments_dict = {}
+    comment_number = 0
+    for comment_key in get_redis_list(key, "comments"):
+        comment = get_comment(comment_key)
+        comments_dict[comment_number] = comment
+
+        author_data = get_author(comment["author"])
+        comments_dict[comment_number]["author"] = author_data
+
+        comment_date = format_comment(comment["datetime"])
+        comments_dict[comment_number]["datetime"] = comment_date
+
+        comment_number += 1
+
+    return jsonify({'comments': comments_dict})
+
+
+def format_comment(date):
+    """Method used to format a datetime to comment date format.
+
+    Parameters:
+        date (datetime.datetime): A python datetime structure.
+    Returns:
+        comment_date (string): A string with comment date format.
+    """
+    comment_date = time.strptime(date, '%Y%d%m%H%M')
+    return time.strftime("%a, %d %b %Y %H:%M", comment_date)
