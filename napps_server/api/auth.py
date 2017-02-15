c926d0cfb9029731c8ea5227f97f35111f954411
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-02-15 17:05:57 -0200

    Fix on endpoint

--- napps_server/api/auth.py
+++ napps_server/api/auth.py
@@ -3,7 +3,7 @@
 # System imports
 
 # Third-party imports
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, Response, jsonify, request
 
 # Local source tree imports
 from napps_server.core.decorators import requires_auth, requires_token
@@ -31,7 +31,7 @@ def napps_auth():
 
 @api.route("/auth/verify/", methods=["POST"])
 @requires_token
-def check_token():
+def check_token(user):
     """Enpoint to check user authentication token.
 
     If the token is a valid token and the token is from the given user, then
@@ -40,4 +40,5 @@ def check_token():
         201 if the token is still valid and is related to the user
         401 otherwise.
     """
-    return 'User authorization correctly verified', 201
+    msg = 'User {} authorization correctly verified'.format(user.username)
+    return Response(msg, 201)
